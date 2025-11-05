c444af8a90855b37aaea0dfdb5ddeea4b6499702
Rahul <rahul955587@gmail.com>
2025-11-05 09:48:13 +0530

    05Oct

--- Java Script/Class Work/DOM(4Nov)/script.js
+++ Java Script/Class Work/DOM(4Nov)/script.js
@@ -1,9 +1,33 @@
-const mainHeading = document.getElementById("title");
+// const mainHeading = document.getElementById("title");
 // mainHeading.textContent="Hello from JavaScript!";
 // mainHeading.textContent="Abhay";
 
 // mainHeading.textContent="{R}{A}{H}{U}{L}";
-const triggerButton=document.getElementById("btn");
-triggerButton.onclick=()=>{
-    mainHeading.textContent="{R}{A}{H}{U}{L}";
-};
+// const triggerButton=document.getElementById("btn");
+// triggerButton.onclick=()=>{
+//     mainHeading.textContent="{R}{A}{H}{U}{L}";
+// };
+
+// const item=document.getElementsByClassName("list-item");
+// console.log(item);
+//
+// item[0].textContent="HEllo";
+// item[1].textContent="Yello";
+// item[2].textContent="22222";
+
+//textContent
+//text html
+//inner text
+
+// const item=document.querySelector(".list-item");//Only fist match return
+// const item1=document.querySelectorAll(".list-item");//All value return
+// item.textContent="22222";
+// console.log(item);
+
+const chess=document.querySelector(".chessboard");
+
+const oddRow=`<div class="square"></div>`.repeat(8);
+const evenRow=`<div class="square1"></div>`.repeat(8);
+const completeHtml=`${oddRow}${evenRow}`.repeat(4);
+chess.innerHTML =completeHtml;
+// console.log(completeHtml);
---
+++ Java Script/Practice/DOM/script.js
@@ -0,0 +1,29 @@
+//DOM manipulation
+
+
+//Excess element
+let heading=document.getElementById("title");
+console.log(heading);
+
+let paragraph=document.getElementsByClassName("para");
+console.log(paragraph);
+
+let p=document.getElementsByTagName("p");
+console.log(p);
+
+//query Selector
+let elements=document.querySelector("p");
+console.log(elements);
+
+let elementsAll=document.querySelectorAll("p");
+console.log(elementsAll);
+
+let a=document.querySelector(".para");
+console.log(a);
+let aa=document.querySelectorAll(".para");
+console.log(a);
+
+let aB=document.querySelector("#btn");
+console.log(aB);
+
+//Parent Child Siblings.
