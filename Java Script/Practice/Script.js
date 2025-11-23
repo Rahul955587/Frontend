ab37ea25e7b00c922e6445ccb9293f350049dd60
Rahul <rahul955587@gmail.com>
2025-11-23 19:43:01 +0530

    move

---
+++ Java Script/Practice/Script.js
@@ -0,0 +1,24 @@
+// console.log("Hello");
+// console.log("Ayush");
+
+//Alert
+
+// alert("Hello Welcome");
+
+// input tag
+
+// const username=prompt("Enter your name");
+// alert(username.toUpperCase()+" "+"Nice to neet you");
+// const age=prompt("Enter your Age:");
+// let message=(age>=18) ? "badda ho gya":"BAchha hai tu abhi";
+// alert(message);
+
+// For variable type
+
+// console.log(typeof age);
+
+// 
+const age=prompt("Age:");
+age>=18
+? document.write("<h1>Adult<h1>")
+: document.write("<h1>Minor<h1>");
